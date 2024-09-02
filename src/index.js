d1db739cc7ef9856bb1dd70e3de615413d831ba7
Vinay Rajput <vinkrins@gmail.com>
2024-09-02 19:20:46 +0000

    updated files locations

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import store from "../store";
 
 import TaskManager from "./TaskManager";
+import store from "./store";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
